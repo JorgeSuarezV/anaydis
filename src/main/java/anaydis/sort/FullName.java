ed16f956c6862c6468e35fb5251a6b031f27ba46
jorge <jorge.sv.primero@gmail.com>
2021-08-26 01:15:23 -0300

    comienzo name sort

--- src/main/java/anaydis/sort/FullName.java
+++ src/main/java/anaydis/sort/FullName.java
@@ -5,6 +5,12 @@ public class FullName {
     private String firstName;
     private String lastName;
 
+    public FullName(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
     public String getFirstName() {
         return firstName;
     }
--- src/main/java/anaydis/sort/NameSort.java
+++ src/main/java/anaydis/sort/NameSort.java
@@ -1,5 +1,16 @@
 package anaydis.sort;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
 public class NameSort {
+
+    BubbleSort sorter = new BubbleSort();
+    Comparator<FullName> comparator = Comparator.comparing(FullName::getFirstName).thenComparing(FullName::getLastName);
+
+
+    public void NameSort(ArrayList<FullName> fullNames) {
+        sorter.sort(comparator, fullNames);
+    }
 
 }
