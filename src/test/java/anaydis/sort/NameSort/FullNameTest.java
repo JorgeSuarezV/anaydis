47a7d851121634c07ced74dc7a6b458c513b22cb
jorge <jorge.sv.primero@gmail.com>
2021-08-26 21:54:12 -0300

    terminado ej 3

--- src/test/java/anaydis/sort/NameSort/FullNameTest.java
+++ src/test/java/anaydis/sort/NameSort/FullNameTest.java
@@ -3,19 +3,24 @@ package anaydis.sort.NameSort;
 import anaydis.sort.Sorter;
 import anaydis.sort.SorterProviderImpl;
 import org.junit.Assert;
+import org.junit.Test;
 
 import java.util.List;
 
 public class FullNameTest {
 
-    public void testFullNameSort(){
+    @Test public void testFullNameSort(){
         FullNameGenerator fullNameGenerator = new FullNameGenerator();
         List<FullName> fullNames = fullNameGenerator.createRandom(50);
         FullNameSort fullNameSort = new FullNameSort();
         fullNameSort.sort(fullNames);
-        for (int i = 0; i < fullNames.size(); i++) {
-            fullNames.get(i).printName();
+        for (int i = 1; i < fullNames.size(); i++) {
+            if (fullNames.get(i-1).getLastName().compareTo(fullNames.get(i).getLastName()) == 0){
+                Assert.assertTrue(fullNames.get(i-1).getFirstName().compareTo(fullNames.get(i).getFirstName()) < 0);
+            }
+            Assert.assertTrue(fullNames.get(i-1).getLastName().compareTo(fullNames.get(i).getLastName()) < 0);
         }
+
     }
 
 }
