cf6d93ca4b4bbb23b3f91055d9486fae1e3670df
jorge <jorge.sv.primero@gmail.com>
2021-09-01 18:35:24 -0300

    fix attept CSVWriter

--- src/test/java/anaydis/sort/performanceTests/SortPerformanceTest.java
+++ src/test/java/anaydis/sort/performanceTests/SortPerformanceTest.java
@@ -4,12 +4,11 @@ import anaydis.sort.SorterProviderImpl;
 import anaydis.sort.SorterType;
 import anaydis.sort.correctnessTests.SorterTest;
 import org.junit.Test;
-import java.io.IOException;
 import java.util.List;
 
 public class SortPerformanceTest extends SorterTest {
     @Test
-    public void performanceTest() throws IOException {
+    public void performanceTest(){
         Integer[] sizes = new Integer[]{100, 500, 1000, 2000, 5000};
         SorterProviderImpl sorterProvider = (SorterProviderImpl) getSorterProvider();
         List<SorterType> sorterTypes = sorterProvider.getAllSorterTypes();
@@ -21,19 +20,5 @@ public class SortPerformanceTest extends SorterTest {
             }
         }
     }
-
-    /*public void performanceTestToCSV() throws IOException {
-        Integer[] sizes = new Integer[]{100, 500, 1000, 2000, 5000};
-        SorterProviderImpl sorterProvider = (SorterProviderImpl) getSorterProvider();
-        List<SorterType> sorterTypes = sorterProvider.getAllSorterTypes();
-        sorterTypes.remove(SorterType.H);
-        SwapsAndComparisonTest test = new SwapsAndComparisonTest();
-        CSVWriter csvWriter = new CSVWriter("sort data");
-        for (SorterType sorterType : sorterTypes) {
-            for (Integer size : sizes) {
-                test.asc_Des_RdmToCSV(csvWriter, sorterType, size, sorterProvider);
-            }
-        }
-    }*/
 }
 
--- src/test/java/anaydis/sort/performanceTests/SwapsAndComparisonTest.java
+++ src/test/java/anaydis/sort/performanceTests/SwapsAndComparisonTest.java
@@ -4,7 +4,6 @@ import anaydis.sort.*;
 import anaydis.sort.correctnessTests.IntegerDataSetGenerator;
 import anaydis.sort.MetricListener;
 import anaydis.sort.SorterProviderImpl;
-import java.io.IOException;
 import java.util.Comparator;
 import java.util.List;
 
@@ -22,31 +21,13 @@ public class SwapsAndComparisonTest{
         printResults(sorterType, "asc", size, listener.getSwaps(), listener.getComparisons());
         listener.reset();
         sorter.sort(Comparator.naturalOrder(), des);
-        printResults(sorterType, "asc", size, listener.getSwaps(), listener.getComparisons());
+        printResults(sorterType, "des", size, listener.getSwaps(), listener.getComparisons());
         listener.reset();
         sorter.sort(Comparator.naturalOrder(), rdm);
-        printResults(sorterType, "asc", size, listener.getSwaps(), listener.getComparisons());
+        printResults(sorterType, "rdm", size, listener.getSwaps(), listener.getComparisons());
     }
 
     public void printResults(SorterType sorterType, String dataSetType, int size, int swaps, int comparisons){
         System.out.println(sorterType + " " + dataSetType + " " + size+ ": swaps; " + swaps + " comparisons; " + comparisons);
     }
-
-    /*public void asc_Des_RdmToCSV(CSVWriter csvWriter, SorterType sorterType, Integer size, SorterProviderImpl sorterProvider) throws IOException {
-        MetricListener listener = new MetricListener();
-        AbstractSorter sorter = (AbstractSorter) sorterProvider.getSorterForType(sorterType);
-        sorter.addSorterListener(listener);
-        IntegerDataSetGenerator dataSetGenerator = new IntegerDataSetGenerator();
-        List<Integer> asc = dataSetGenerator.createAscending(size);
-        List<Integer> des = dataSetGenerator.createDescending(size);
-        List<Integer> rdm = dataSetGenerator.createRandom(size);
-        sorter.sort(Comparator.naturalOrder(), asc);
-        csvWriter.write(sorterType + ",asc," + size + "," + listener.getSwaps() + "," + listener.getComparisons());
-        listener.reset();
-        sorter.sort(Comparator.naturalOrder(), des);
-        csvWriter.write(sorterType + ",des," + size + "," + listener.getSwaps() + "," + listener.getComparisons());
-        listener.reset();
-        sorter.sort(Comparator.naturalOrder(), rdm);
-        csvWriter.write(sorterType + ",rdm," + size + "," + listener.getSwaps() + "," + listener.getComparisons());
-    }*/
 }
