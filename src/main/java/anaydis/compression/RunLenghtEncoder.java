7cbd86aeb9a0a35af0a92ad165476f24f0ccb325
jorge <jorge.sv.primero@gmail.com>
2021-11-19 19:02:54 -0300

    Inmutable Binary Tree

---
+++ src/main/java/anaydis/compression/RunLenghtEncoder.java
@@ -0,0 +1,56 @@
+package anaydis.compression;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class RunLenghtEncoder implements Compressor{
+
+    @Override
+    public void encode(@NotNull InputStream input, @NotNull OutputStream output) throws IOException {
+        int previous = input.read();
+        int actual = input.read();
+        int count = 1;
+        if (actual != -1) {
+            while (actual != -1) {
+                if (previous == actual && count < 256) {
+                    count++;
+                } else {
+                    if(count != 1){
+                        output.write(0xFF);
+                        output.write(previous);
+                    }
+                    output.write(count);
+                    count = 1;
+                }
+                previous = actual;
+                actual = input.read();
+            }
+        }
+    }
+
+    @Override
+    public void decode(@NotNull InputStream input, @NotNull OutputStream output) throws IOException {
+        int amount = 1;
+        boolean amountNext = false;
+        int current = input.read();
+        while (current!=-1){
+            if(amountNext) {
+                amount = current;
+                amountNext = false;
+            }
+            else if(current == 0xFF){
+                amountNext = true;
+            }
+            else {
+                for (int i = 0; i < amount; i++) {
+                    output.write(current);
+                }
+                amount = 1;
+            }
+            current = input.read();
+        }
+    }
+}
